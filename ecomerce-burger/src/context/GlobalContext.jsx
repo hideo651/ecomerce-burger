31500504b9d531258e9e8f37bd8dae4455ca2c24
hideo651 <hideo651@msn.com>
2024-04-05 14:29:36 -0300

    pequenas correcoes

--- ecomerce-burger/src/context/GlobalContext.jsx
+++ ecomerce-burger/src/context/GlobalContext.jsx
@@ -8,6 +8,7 @@ export const GlobalStorage = ({ children }) => {
   const [dados, setDados] = React.useState([]);
   const [carrinho, setCarrinho] = React.useState([]);
   const [product, setProduct] = React.useState([]);
+  console.log("");
 
   React.useEffect(() => {
     setDados(produtos);
